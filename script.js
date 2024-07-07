b3a4281957425dc40ff965d36e97285d237e9916
Mashrur Elahi <elahi@macbook-pro-2.mynetworksettings.com>
2024-07-07 16:48:55 -0400

    BigPortfolioPush

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let currentIndex = 0;
 const items = document.querySelectorAll(".carousel-item");
 const totalItems = items.length;
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls'); 
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
 
 function showNextSlide() {
   if (currentIndex < totalItems - 1) {
@@ -18,3 +22,30 @@ function updateCarousel() {
 }
 
 setInterval(showNextSlide, 3000);
+
+
+
+class Carousel {
+
+
+  constructor(container, items, controls) {
+    this.carouselContainer = container; 
+    this.carouselControls = controls; 
+    this.carouselArray = [...items];
+  }
+
+  updateGallery(){
+    this.carouselArray.forEach(el => {
+      el.classList.remove('gallery-item-1');
+      el.classList.remove('gallery-item-2');
+      el.classList.remove('gallery-item-3');
+      el.classList.remove('gallery-item-4');
+      el.classList.remove('gallery-item-5');
+    });
+
+    this.carouselArray.slice(0, 5).forEach((el, i) => {
+      el.classList.add(`gallery-item-${i+1}`);
+    });
+  }
+
+}
